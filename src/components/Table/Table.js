46c6fdfba19bcf54972a37c93bdeb42a09050416
Daria Dorna <perelka4@onet.pl>
2023-09-11 20:50:06 +0200

    RWD

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -14,7 +14,7 @@ const Table = (props) => {
         <div className="row justify-contetnt-between align-items-center">
           <h2 className="col-2 m-4">Table {props.id} </h2>
           <p className="col-2 m-5"><span className={styles.status}>Status:</span> {props.status} </p>
-          <div className={clsx("col-6", styles.buttoncon)}><Link to={'/table/' + props.id} ><button className={clsx("btn-default, btn-lg", styles.btnshow)}>Show more</button></Link></div>
+          <div className={clsx("col-6, col-sm-3", styles.buttoncon)}><Link to={'/table/' + props.id} ><button className={clsx("btn-default, btn-lg", styles.btnshow)}>Show more</button></Link></div>
 
         </div>
       </li>
